d8a7b6e0a04600156a7bd0d508f4e57c7a7b2e53
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2016-06-18 20:09:24 +0530

    add buttons to menu

--- menu.py
+++ menu.py
@@ -10,4 +10,73 @@ class Menu:
 
     def render(self):
         self.screen.blit(self.background, (0, 0))
+
+        (width, height) = self.screen.get_size()
+
+        pygame.draw.rect(
+            self.screen,
+            (255, 0, 0),
+            (
+                (345 / 1024.0) * width,
+                (94 / 768.0) * height,
+                (143 / 1024.0) * width,
+                (143 / 768.0) * height
+            )
+        )
+
+        pygame.draw.rect(
+            self.screen,
+            (255, 0, 0),
+            (
+                (345 / 1024.0) * width,
+                (257 / 768.0) * height,
+                (143 / 1024.0) * width,
+                (143 / 768.0) * height
+            )
+        )
+
+        pygame.draw.rect(
+            self.screen,
+            (255, 0, 0),
+            (
+                (345 / 1024.0) * width,
+                (420 / 768.0) * height,
+                (143 / 1024.0) * width,
+                (143 / 768.0) * height
+            )
+        )
+
+        pygame.draw.rect(
+            self.screen,
+            (255, 0, 0),
+            (
+                (184 / 1024.0) * width,
+                (420 / 768.0) * height,
+                (143 / 1024.0) * width,
+                (143 / 768.0) * height
+            )
+        )
+
+        pygame.draw.rect(
+            self.screen,
+            (255, 0, 0),
+            (
+                (22 / 1024.0) * width,
+                (420 / 768.0) * height,
+                (143 / 1024.0) * width,
+                (143 / 768.0) * height
+            )
+        )
+
+        pygame.draw.rect(
+            self.screen,
+            (255, 0, 0),
+            (
+                (345 / 1024.0) * width,
+                (94 / 768.0) * height,
+                (143 / 1024.0) * width,
+                (143 / 768.0) * height
+            )
+        )
+
         pygame.display.update()
