62a65e11409c99952ae897dac1fd9a6ee7aea65c
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2016-06-18 20:48:59 +0530

    add Text class to easily render text

--- menu.py
+++ menu.py
@@ -1,4 +1,9 @@
 import pygame
+from text import Text
+
+pygame.init()
+
+ROBOTO = pygame.font.Font('fonts/roboto/Roboto-Regular.ttf', 20)
 
 
 class Menu:
@@ -13,7 +18,8 @@ class Menu:
 
         (width, height) = self.screen.get_size()
 
-        pygame.draw.rect(
+        text = Text('Game 1', ROBOTO)
+        btn = pygame.draw.rect(
             self.screen,
             (255, 0, 0),
             (
@@ -23,6 +29,7 @@ class Menu:
                 (143 / 768.0) * height
             )
         )
+        text.render(self.screen, btn)
 
         pygame.draw.rect(
             self.screen,
@@ -63,17 +70,6 @@ class Menu:
             (
                 (22 / 1024.0) * width,
                 (420 / 768.0) * height,
-                (143 / 1024.0) * width,
-                (143 / 768.0) * height
-            )
-        )
-
-        pygame.draw.rect(
-            self.screen,
-            (255, 0, 0),
-            (
-                (345 / 1024.0) * width,
-                (94 / 768.0) * height,
                 (143 / 1024.0) * width,
                 (143 / 768.0) * height
             )
---
+++ text.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+
+
+class Text:
+    def __init__(self, text, font, color=(0, 0, 0), align='center center'):
+        self.text_surface = font.render(text, True, color)
+        self.align = align
+
+    def render(self, screen, button_rect):
+        (x, y) = (button_rect.x, button_rect.y)
+        (w, h) = (button_rect.w, button_rect.h)
+        text_rect = self.text_surface.get_rect()
+
+        if self.align == 'top left':
+            text_rect.topleft = (x, y)
+        if self.align == 'top center':
+            text_rect.midtop = (x + w / 2, y)
+        if self.align == 'top right':
+            text_rect.topright = (x + w, y)
+
+        if self.align == 'center left':
+            text_rect.midleft = (x, y + h / 2)
+        if self.align == 'center center':
+            text_rect.center = (x + w / 2, y + h / 2)
+        if self.align == 'center right':
+            text_rect.midleft = (x + w, y + h / 2)
+
+        if self.align == 'bottom left':
+            text_rect.bottomleft = (x, y + h)
+        if self.align == 'bottom center':
+            text_rect.midbottom = (x + w / 2, y + h)
+        if self.align == 'bottom right':
+            text_rect.bottomright = (x + w, y + h)
+
+        screen.blit(self.text_surface, text_rect)
