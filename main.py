2b7be7bbd4a44b476b0a75fd72456310adf6d039
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2016-06-18 16:34:33 +0530

    add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+
+class Astrocytes:
+    def __init__(self):
+        size = (width, height) = (512, 384)
+        self.screen = pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.HWSURFACE)
+        self.background = pygame.Surface(self.screen.get_size())
+
+        pygame.display.set_caption('Astrocytes')
+
+    def run(self):
+        while True:
+            event = pygame.event.wait()
+            print event
+
+
+if __name__ == '__main__':
+    pygame.init()
+
+    game = Astrocytes()
+    game.run()
