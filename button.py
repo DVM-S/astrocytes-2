e6f26866aea7cefdbeeb196472f1f4b352a53634
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2016-06-23 03:30:45 +0530

    add button highlighting

--- button.py
+++ button.py
@@ -15,18 +15,30 @@ class Button:
         self.h = h
 
         self.bg = bg
-        self.fg = fg
+        self.fg = fg or bg
         self.text = text
 
         self.left_click_callback = on_left_click
         self.middle_click_callback = on_middle_click
         self.right_click_callback = on_right_click
 
+        self.is_focused = False
+
         EVENT_STREAM.subscribe(self.event_handler)
 
     def event_handler(self, e):
         if e.type == pygame.MOUSEBUTTONDOWN:
             self.on_click(e)
+        elif e.type == pygame.MOUSEMOTION:
+            if self.focused(e):
+                self.is_focused = True
+            else:
+                self.is_focused = False
+
+    def focused(self, e):
+        return (
+            (self.x < e.pos[0] and e.pos[0] < self.x + self.w) and
+            (self.y < e.pos[1] and e.pos[1] < self.y + self.h))
 
     def on_click(self, e):
         if (
@@ -48,16 +60,21 @@ class Button:
     def render(self, pos):
         (self.x, self.y) = pos
 
-        if isinstance(self.bg, pygame.Surface):
-            surface = pygame.transform.scale(self.bg, (int(self.w), int(self.h)))
+        if self.is_focused:
+            icon = self.fg
+        else:
+            icon = self.bg
+
+        if isinstance(icon, pygame.Surface):
+            surface = pygame.transform.scale(icon, (int(self.w), int(self.h)))
             surface_rect = surface.get_rect()
             surface_rect.topleft = (self.x, self.y)
             surface_rect.bottomright = (self.x + self.w, self.y + self.h)
 
             SCREEN.blit(surface, surface_rect)
 
-        elif isinstance(self.bg, tuple) and len(self.bg) == 3:
-            surface_rect = pygame.draw.rect(SCREEN, self.bg, (self.x, self.y, self.w, self.h))
+        elif isinstance(icon, tuple):
+            surface_rect = pygame.draw.rect(SCREEN, icon, (self.x, self.y, self.w, self.h))
 
         if self.text:
             self.text.render(surface_rect)
