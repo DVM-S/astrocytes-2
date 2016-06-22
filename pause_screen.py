4453aad14abaa6474d68f72c9fa39d1715eb9225
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2016-06-23 02:25:07 +0530

    add button

--- pause_screen.py
+++ pause_screen.py
@@ -1,9 +1,14 @@
 from utils import (
-    COLOR_RED,
+    COLOR_BLACK,
+    COLOR_WHITE,
+    FONT_ROBOTO,
     SCREEN,
     SCREEN_SIZE)
 
 import pygame
+
+from button import Button
+from text import Text
 
 
 class PauseScreen:
@@ -11,6 +16,15 @@ class PauseScreen:
         self.hidden = True
         self.width = 0
         self.max_witdh = 200
+
+        self.init_buttons()
+
+    def init_buttons(self):
+        self.btn_back = Button(
+            (0, 0, 100, 100),
+            bg=(0, 0, 0),
+            fg=(100, 100, 100),
+            text=Text('Back', FONT_ROBOTO, COLOR_WHITE))
 
     def hide(self):
         self.width = 0
@@ -25,6 +39,7 @@ class PauseScreen:
             SCREEN.blit(tint, (0, 0))
             self.hidden = False
 
+        self.btn_back.render()
         SCREEN.fill(
             (100, 100, 100),
             (SCREEN_SIZE.W - self.width, 0, self.width, SCREEN_SIZE.H))
