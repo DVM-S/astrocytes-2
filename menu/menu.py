f03dbe48b9e26ad6dd3c166e420e598060338ccd
SHIRSH ZIBBU <zibs.shirsh@gmail.com>
2016-07-31 00:33:01 +0530

    update menu presentation

--- menu/menu.py
+++ menu/menu.py
@@ -37,7 +37,6 @@ class Menu:
                 (143 / 768.0) * SCREEN_SIZE.H),
             bg=pygame.image.load('menu/g1.png'),
             fg=pygame.image.load('menu/g1f.png'),
-            text=Text('Game 1'),
             on_left_click=lambda: load_game(1)
         )
 
@@ -46,7 +45,6 @@ class Menu:
                 (143 / 768.0) * SCREEN_SIZE.H),
             bg=pygame.image.load('menu/g2.png'),
             fg=pygame.image.load('menu/g2f.png'),
-            text=Text('Game 2'),
             on_left_click=lambda: load_game(2)
         )
 
@@ -55,7 +53,6 @@ class Menu:
                 (143 / 768.0) * SCREEN_SIZE.H),
             bg=pygame.image.load('menu/g3.png'),
             fg=pygame.image.load('menu/g3f.png'),
-            text=Text('Game 3'),
             on_left_click=lambda: load_game(3)
         )
 
@@ -64,7 +61,6 @@ class Menu:
                 (143 / 768.0) * SCREEN_SIZE.H),
             bg=pygame.image.load('menu/g4.png'),
             fg=pygame.image.load('menu/g4f.png'),
-            text=Text('Game 4')
         )
 
         self.btn_5 = Button('menu', (
@@ -72,38 +68,26 @@ class Menu:
                 (143 / 768.0) * SCREEN_SIZE.H),
             bg=pygame.image.load('menu/g5.png'),
             fg=pygame.image.load('menu/g5f.png'),
-            text=Text('Game 5')
-        )
-
-        self.profile = Button('menu', (
-                (467 / 1024.0) * SCREEN_SIZE.W,
-                (92 / 768.0) * SCREEN_SIZE.H),
-            bg=pygame.image.load('menu/p.png'),
-            fg=pygame.image.load('menu/pf.png'),
-            text=Text('VIEW MY PROFILE')
         )
 
     def render(self):
         SCREEN.blit(self.bg, (0, 0))
 
         self.game.render((
-            (22 / 1024.0) * SCREEN_SIZE.W,
-            (94 / 768.0) * SCREEN_SIZE.H))
+            ((22 + 10) / 1024.0) * SCREEN_SIZE.W,
+            ((94 + 60) / 768.0) * SCREEN_SIZE.H))
         self.btn_1.render((
-            (345 / 1024.0) * SCREEN_SIZE.W,
-            (94 / 768.0) * SCREEN_SIZE.H))
+            ((345 + 10) / 1024.0) * SCREEN_SIZE.W,
+            ((94 + 60) / 768.0) * SCREEN_SIZE.H))
         self.btn_2.render((
-            (345 / 1024.0) * SCREEN_SIZE.W,
-            (257 / 768.0) * SCREEN_SIZE.H))
+            ((345 + 10) / 1024.0) * SCREEN_SIZE.W,
+            ((257 + 60) / 768.0) * SCREEN_SIZE.H))
         self.btn_3.render((
-            (345 / 1024.0) * SCREEN_SIZE.W,
-            (420 / 768.0) * SCREEN_SIZE.H))
+            ((345 + 10) / 1024.0) * SCREEN_SIZE.W,
+            ((420 + 60) / 768.0) * SCREEN_SIZE.H))
         self.btn_4.render((
-            (184 / 1024.0) * SCREEN_SIZE.W,
-            (420 / 768.0) * SCREEN_SIZE.H))
+            ((184 + 10) / 1024.0) * SCREEN_SIZE.W,
+            ((420 + 60) / 768.0) * SCREEN_SIZE.H))
         self.btn_5.render((
-            (22 / 1024.0) * SCREEN_SIZE.W,
-            (420 / 768.0) * SCREEN_SIZE.H))
-        self.profile.render((
-            (22 / 1024.0) * SCREEN_SIZE.W,
-            (582 / 768.0) * SCREEN_SIZE.H))
+            ((22 + 10) / 1024.0) * SCREEN_SIZE.W,
+            ((420 + 60) / 768.0) * SCREEN_SIZE.H))
